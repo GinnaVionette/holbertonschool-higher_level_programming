eabec45b9ae8e4f724e8d14aff474822aaa85cad
GinnaVionette <9011@holbertonstudents.com>
2024-05-07 12:34:53 -0400

    create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("object-oriented programming" if "object-oriented programming" in "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax" else "")
+print("object-oriented programming" * ("object-oriented programming" in "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"))
 
