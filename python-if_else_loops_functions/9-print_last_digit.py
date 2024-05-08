a71cccdead1e1520b6f50623f8213ab19ba68359
GinnaVionette <9011@holbertonstudents.com>
2024-05-08 14:16:16 -0400

    fixing code for and pushing main file for exercise 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    last_digit = number % 10
-    print(last_digit)
-    return last_digit
+    last = abs(number) % 10
+    print("{}".format(last), end="")
+    return last
