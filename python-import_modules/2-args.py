de207835ff586404ed4139b6c9d5a0cca59fa740
GinnaVionette <9011@holbertonstudents.com>
2024-05-10 11:10:18 -0400

    fixing code for task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,12 +5,8 @@ from sys import argv
 if __name__ == "__main__":
     num_args = len(argv) - 1
 
-    print("Number of argument{}: ".format("s" if num_args != 1 else ""), end="")
-    print("argument" if num_args == 1 else "arguments", end="")
-    print(":")
-    
-    if num_args == 0:
-        print(".")
-    else:
+    print("{} argument{}:".format(num_args, "s" if num_args != 1 else ""))
+
+    if num_args > 0:
         for i, arg in enumerate(argv[1:], start=1):
             print("{}: {}".format(i, arg))
