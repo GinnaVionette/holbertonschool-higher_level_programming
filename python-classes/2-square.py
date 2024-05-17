fb5f3aae79ccda3fba43eb9016fa765e6cf1c18b
GinnaVionette <9011@holbertonstudents.com>
2024-05-17 16:58:06 -0400

    fixing code

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 # 2-square.py
-"""Size validation: """
+"""Define a class Square."""
 
 
-class Square(object):
-    """class variable size"""
+class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """initialize size"""
-        if type(size) != int:
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
