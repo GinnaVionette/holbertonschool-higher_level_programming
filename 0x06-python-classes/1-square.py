f3d3755f1f791c7efa6779d446be0329ec76000d
GinnaVionette <9011@holbertonstudents.com>
2024-05-20 08:46:09 -0400

    rename

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Private instance attribute: """
+
+
+class Square(object):
+    """class variable size"""
+    def __init__(self, size):
+        """initialize size"""
+        self.__size = size
