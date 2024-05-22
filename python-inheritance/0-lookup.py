a400cb6d054d12b812f7bfc99160ee710de447bd
GinnaVionette <9011@holbertonstudents.com>
2024-05-22 10:34:34 -0400

    fixing code and submitting exercise 1 code and text file

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+    """
 
-
-def safe_print_list(my_list=[], x=0):
-    idx = 0
-
-    try:
-        for i in my_list:
-            if idx < x:
-                print('{}'.format(my_list[idx]), end='')
-                idx += 1
-
-        print()
-    except TypeError:
-        pass
-    finally:
-        return idx
+    return dir(obj)
